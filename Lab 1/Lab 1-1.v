62784cfd7e0e9c2489a935e41502c456ec7a8d83
stormteeth <55481909+stormteeth@users.noreply.github.com>
2021-09-22 15:14:46 +0800

    Add files via upload

---
+++ Lab 1/Lab 1-1.v
@@ -0,0 +1,13 @@
+module lab2();
+reg[31:0] data;
+integer i;
+
+initial begin
+	for(i=0;i<32;i=i+2)
+		data[i]=0;
+	for(i=1;i<32;i=i+2)
+		data[i]=1;
+	for(i=0;i<32;i=i+1)
+		$display("date[%d]=%d\n",i,data[i]);
+end
+endmodule
