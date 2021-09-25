b8b88358cd592a60d644883cb99660a4468786f4
stormteeth <55481909+stormteeth@users.noreply.github.com>
2021-09-25 16:24:29 +0800

    Update 8_bit_counter.v

--- Lab 5/8_bit_counter.v
+++ Lab 5/8_bit_counter.v
@@ -3,28 +3,24 @@ output q;
 reg clock;
 reg [7:0] q;
 integer i;
-initial
-   begin:block1
-      i=0;
-      clock=1'b1;
-      q=0;
-   while(1)
-      begin
-   if(i>=70)
-   disable block1;
-   #5 clock=~clock;
+initial begin:block1
+   i=0;
+   clock=1'b1;
+   q=0;
+   while(1)begin
+      if(i>=70)
+         disable block1;
+         #5 clock=~clock;
    end
 end
-always@(posedge clock)
-   begin
-      i=i+1;
+always@(posedge clock)begin
+   i=i+1;
    if(i<67&&i>=5)
       q=q+1;
    else
       q=q;
    end
-initial
-begin
+initial  begin
 $monitor($time,"number=%d,counter=%d",i,q);
 end
 endmodule
