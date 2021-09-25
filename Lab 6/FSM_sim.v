d857d4e17d9ea06081b5a2a15be1d6595d240e3e
stormteeth <55481909+stormteeth@users.noreply.github.com>
2021-09-25 18:42:20 +0800

    Update FSM_sim.v

--- Lab 6/FSM_sim.v
+++ Lab 6/FSM_sim.v
@@ -6,16 +6,16 @@ Morre_FSM try(Out,state,clock,reset,in);
 initial clock=1'b0;
 always #10 clock=~clock;
 initial begin
-reset=1'b0;
-#10 reset=1'b1;in=1'b0;
-#10 reset=1'b0;//S0
-#20 in=1'b0;//S1
-#20 in=1'b1;//S2
-#20 in=1'b1;//S3
-#20 in=1'b0;//S4
-#20 $finish;
+    reset=1'b0;
+    #10 reset=1'b1;in=1'b0;
+    #10 reset=1'b0;//S0
+    #20 in=1'b0;//S1
+    #20 in=1'b1;//S2
+    #20 in=1'b1;//S3
+    #20 in=1'b0;//S4
+    #20 $finish;
 end
 initial begin
-    $monitor($time,"in=%b,out=%b,state=%b",in,Out,state);
+    $monitor($time,"state=%b,in=%b,out=%b,nextstate=%b",state,in,Out,nextstate);
 end
 endmodule
