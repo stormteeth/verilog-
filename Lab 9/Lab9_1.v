01e7675bfe4dd3870b917d8352f2c2333e5dcb10
stormteeth <55481909+stormteeth@users.noreply.github.com>
2021-09-28 16:25:23 +0800

    Update Lab9_1.v

--- Lab 9/Lab9_1.v
+++ Lab 9/Lab9_1.v
@@ -1,12 +1,9 @@
 module test;
 reg[7:0]memory[0:7];
 integer i;
-initial
-begin
-$readmemh("init.dat",memory);
-
-for(i=0;i<8;i=i+1)
-
-$display("Memory[%0d]=%h",i,memory[i]);
+initial begin
+   $readmemh("init.dat",memory);
+   for(i=0;i<8;i=i+1)
+      $display("Memory[%0d]=%h",i,memory[i]);
 end
 endmodule
