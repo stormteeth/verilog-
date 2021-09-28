eb219f8b09a31f71908da32fdbaa8b502d1567db
stormteeth <55481909+stormteeth@users.noreply.github.com>
2021-09-28 16:02:29 +0800

    Update Even_parity.v

--- Lab 8/Even_parity.v
+++ Lab 8/Even_parity.v
@@ -7,15 +7,13 @@ initial
    clock=1'b0;
 always
    #5 clock=~clock;
-always@(posedge clock)
-begin
-i=i+1;
-a=7;
-if (i==3)
-begin
-separatly(b,a);
-$display("%b(%d)¥[¤W­×¥¿¼Æ:%d«áÅÜ¬°°¸¦P¦ì",a,a,b);
-end
+always@(posedge clock)begin
+   i=i+1;
+   a=7;
+   if(i==3)begin
+      separatly(b,a);
+      $display("%b(%d)åŠ ä¸Šä¿®æ­£æ•¸:%då¾Œè®Šç‚ºå¶åŒä½",a,a,b);
+   end
 end
 //------------------
 initial
@@ -25,11 +23,11 @@ output b;
 input [15:0]a;
 integer c;
 begin
-  c=a[0]^a[1]^a[2]^a[3]^a[4]^a[5]^a[6]^a[7]^a[8]^a[9]^a[10]^a[11]^a[12]^a[13]^a[14]^a[15];
- if(c==0)
-  b=0;
-else
-  b=1;
+   c=a[0]^a[1]^a[2]^a[3]^a[4]^a[5]^a[6]^a[7]^a[8]^a[9]^a[10]^a[11]^a[12]^a[13]^a[14]^a[15];
+   if(c==0)
+      b=0;
+   else
+      b=1;
 end
 endtask
 endmodule
