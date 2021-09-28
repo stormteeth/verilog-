46ebe3c265ffd0a3cc4b3f60f58534b9e74eb909
stormteeth <55481909+stormteeth@users.noreply.github.com>
2021-09-28 15:44:01 +0800

    Update Multiply.v

--- Lab 8/Multiply.v
+++ Lab 8/Multiply.v
@@ -2,40 +2,35 @@ module operation;
 reg [3:0] a;
 reg [3:0] b;
 reg [7:0] total;
-initial
-begin
- a=3;
- b=5;
-total=multi_opter(a,b);
-$display("total=%d",total);
+initial begin
+   a=3;
+   b=5;
+   total=multi_opter(a,b);
+   $display("total=%d",total);
 end
-initial
-begin
- a=4;
- b=9;
-total=multi_opter(a,b);
-$display("total=%d",total);
+initial begin
+   a=4;
+   b=9;
+   total=multi_opter(a,b);
+   $display("total=%d",total);
 end
-initial
-begin
- a=6;
- b=4;
-total=multi_opter(a,b);
-$display("total=%d",total);
+initial begin
+   a=6;
+   b=4;
+   total=multi_opter(a,b);
+   $display("total=%d",total);
 end
-initial
-begin
- a=9;
- b=8;
-total=multi_opter(a,b);
-$display("total=%d",total);
+initial begin
+   a=9;
+   b=8;
+   total=multi_opter(a,b);
+   $display("total=%d",total);
 end
-initial
-begin
- a=7;
- b=3;
-total=multi_opter(a,b);
-$display("total=%d",total);
+initial begin
+   a=7;
+   b=3;
+   total=multi_opter(a,b);
+   $display("total=%d",total);
 end
 function [7:0] multi_opter;
 input [3:0]a;
